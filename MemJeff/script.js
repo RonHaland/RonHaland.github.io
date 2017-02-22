a1b12766ecb42a8a309f47e3d6ed6fb010a02c9b
Ronny Håland <ronnyhaland@gmail.com>
2017-02-22 21:13:26 +0100

    Update

--- MemJeff/script.js
+++ MemJeff/script.js
@@ -2,17 +2,38 @@ $(document).ready(function() {
   playerTurn = 1, Score = [], card1 = -1, card2 = -1, card1ind = -1, card2ind = -1;
   Score[1] = 0, Score[2] = 0;
   valueArray = [];
-  animationReady = 1;
+  animationReady = 1, timer = 0, totalFlips = 0, seconds = 0, minutes = 0, hours = 0;
   $("#welcome").click(function(){
     $("#cardboard").css("display","block");
     $("#scoreboard").css("display","block");
-    $("#name1").css("color","red");
+    $("#stats").css("display","block");
+    $("#name1").css("color","#FA2");
     $(this).css("display","none");
     deal(4);
+    if (!timer){
+      setInterval(function(){ myTimer() }, 1000);
+    }else{
+      minutes = 0;
+      seconds = 0;
+    }
     $("#cardboard").on("click", ".card", function() {
       turnCard($(this));
     });
   });
+  function myTimer() {
+    timer = 1
+    seconds++;
+    if (seconds >= 60){
+      minutes++;
+      seconds = 0;
+    }
+    if (minutes >= 60){
+      hours++;
+      minutes = 0;
+    }
+    $("#time").text("Time: " + minutes + "m, " + seconds + "s");
+  }
+
   function deal(x){
     $("#cardboard").empty();
     var sizeCols = x;
@@ -62,41 +83,62 @@ function turnCard(card){
   }
 }
 function turnChange(){
-  setTimeout(function(){
-  c1 = $("#x"+card1ind);
-  c2 = $("#x"+card2ind);
-  if (card1 === card2){
-    Score[playerTurn] += 1;
-    c1.addClass('done');
+    setTimeout(function(){
+    c1 = $("#x"+card1ind);
+    c2 = $("#x"+card2ind);
+    if (card1 === card2){
+      Score[playerTurn] += 1;
+      c1.addClass('done');
 
-    c2.addClass('done');
+      c2.addClass('done');
 
-      c1.css("opacity","0.5");
-      c2.css("opacity","0.5");
+        c1.css("opacity","0.5");
+        c2.css("opacity","0.5");
 
-    $("#p"+playerTurn).text("" + Score[playerTurn]);
-  }
-  else if (playerTurn === 1){playerTurn = 2; $("#name2").css("color","red");$("#name1").css("color","white");}
-  else if(playerTurn === 2){playerTurn = 1; $("#name1").css("color","red");$("#name2").css("color","white");}
-  if (card1 !== card2){
-    flipAnimation(c1,1);
-    flipAnimation(c2,1);
-  }
-  card1 = -1;
-  card2 = -1;
-  card1ind = -1;
-  card2ind = -1;
-  }, 1500);
+      $("#p"+playerTurn).text("" + Score[playerTurn]);
+    }
+    else if (playerTurn === 1){playerTurn = 2; $("#name2").css("color","#FA2");$("#name1").css("color","white");}
+    else if(playerTurn === 2){playerTurn = 1; $("#name1").css("color","#FA2");$("#name2").css("color","white");}
+    if (card1 !== card2){
+      flipAnimation(c1,1);
+      flipAnimation(c2,1);
+    }
+    if ((Score[1] + Score[2]) >= (valueArray.length/2)){
+      $("#cardboard").css("display","none");
+      $("#scoreboard").css("display","none");
+      $("#stats").css("display","none");
+      $("#welcome").css("display","block");
+      if (Score[1] > Score[2]){
+        $("#welcome").html("Congratulations! <br/> Player 1 Wins! <br/> Click to play again");
+      } else if (Score[1] < Score[2]){
+        $("#welcome").html("Congratulations! <br/> Player 2 Wins! <br/> Click to play again");
+      } else {
+        $("#welcome").html("WOW! <br /> Its a draw! <br/> Click to play again");
+      }
+      Score[1] = 0;
+      Score[2] = 0;
+      flips = 0;
+
+
+    }
+    card1 = -1;
+    card2 = -1;
+    card1ind = -1;
+    card2ind = -1;
+    }, 1500);
 }
 function flipAnimation(card, reverse){
   if (!reverse){
     var jeff = card.attr('class').split(' ');
     jeff  = jeff[jeff.length-1];
     animationReady = 0;
+
     card.animate({width:0, marginLeft:40, marginRight:40}, 500, function(){
       card.css('background-image', 'url(images/Jeff' + jeff + '.jpg)');
       card.animate({width:70, marginLeft:5, marginRight:5}, 500);
       animationReady = 1;
+      totalFlips++;
+      $("#flips").text("Total Flips: " + totalFlips);
     });
   } else{
     setTimeout(function(){
@@ -106,7 +148,7 @@ function flipAnimation(card, reverse){
       card.animate({width:70, marginLeft:5, marginRight:5}, 500);
       animationReady = 1;
     });
-  }, 1500);
+  }, 1000);
   }
 }
 //Shuffle an array
